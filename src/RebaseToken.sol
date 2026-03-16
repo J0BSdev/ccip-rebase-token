7489e572cd1a6f68a9a91b6d36d538842d93348d
Lovro <lovro.posel79@gmail.com>
2026-03-16 20:44:01 +0100

    Initial commit

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -4,41 +4,64 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-
 /*
 *title: RebaseToken
 *author; Lovro Posel
-* @notice 
-* @notice 
+* @notice
+* @notice
 */
 
-
-
 contract RebaseToken is ERC20 {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimeStamp;
 
-error RebaseToken__InterestRateCanOnlydecrease(uint256 oldInterestRate, uint256 newInterestRate);
+    event InterestRateSet(uint256 newInterestRate);
 
-uint256 private s_interestRate = 5e10;
+    constructor() ERC20("RebaseToken", "RBK") {}
 
-event InterestRateSet(uint256 newInterestRate);
-
-
-    constructor() ERC20("RebaseToken", "RBK") {
-
-
-        
-
+    function setInterestRate(uint256 _newInterestRate) external {
+        //set the new interest rate
+        if (_newInterestRate > s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
+        }
+        s_interestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
     }
 
-function setInterestRate(uint256 _newInterestRate) external {
-    //set the new interest rate
-    if (_newInterestRate > s_interestRate) {
-        revert RebaseToken__InterestRateCanOnlydecrease(s_interestRate, _newInterestRate);
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        _mint(_to, _amount);
     }
-    s_interestRate = _newInterestRate;
-    emit InterestRateSet(_newInterestRate);
 
-}
+    function balanceOf(address _user) public view override returns (uint256) {
+        return super.balanceOf(_user) * _calculateUserAccumulatedInterestSincelastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    function _calculateUserAccumulatedInterestSincelastUpdate(address _user)
+        internal
+        view
+        returns (uint256 linearInterest)
+    {
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimeStamp[_user];
+        linearInterest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        //find the current balacne of rebse tokens that have been minted to user-> principle
+        s_userLastUpdatedTimeStamp[_user] = block.timestamp;
+    }
+
+    /*
+    *@notice Get the interest rate for the user
+
+
+    */
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate[_user];
+    }
 }
